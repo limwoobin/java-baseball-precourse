1dca5f5fae698781e1489992a7bc2e3c97f81883
limwoobin <drogba02@naver.com>
2022-04-16 20:07:25 +0900

    <feature>: 게임을 시작하는 비지니스 로직을 작성하라
    - 게임진행에 대한 행위는 BaseballGame.java 에서 담당한다

--- src/main/java/baseball/game/BaseballGame.java
+++ src/main/java/baseball/game/BaseballGame.java
@@ -1,5 +1,27 @@
 package baseball.game;
 
+import baseball.domain.BaseballNumbers;
+import baseball.domain.GameResult;
+import baseball.domain.input.InputNumbers;
+import baseball.domain.score.impl.BallScoreFunction;
+import baseball.domain.score.impl.StrikeScoreFunction;
+import baseball.view.PrintGameResult;
+import baseball.view.PrintUserInput;
+
 public class BaseballGame {
     public static final int BASEBALL_GAME_NUMBER_SIZE = 3;
+
+    public static void start() {
+        BaseballNumbers baseballNumbers = new BaseballNumbers();
+        InputNumbers inputNumbers = new InputNumbers();
+        GameResult gameResult = new GameResult(new StrikeScoreFunction(), new BallScoreFunction());
+
+        while (true) {
+            PrintUserInput.printMessage();
+            inputNumbers = inputNumbers.receiveInputNumbers();
+
+            gameResult.calculateScore(inputNumbers, baseballNumbers);
+            PrintGameResult.printMessage(gameResult);
+        }
+    }
 }
